29220e96650014081c9873c3ffcbdafb4909e119
班子涵 <1147092872@qq.com>
2019-04-19 17:18:47 +0800

    新增店铺

--- src/main/java/com/poison/es/service/group/GroupMapper.java
+++ src/main/java/com/poison/es/service/group/GroupMapper.java
@@ -2,6 +2,7 @@ package com.poison.es.service.group;
 
 import com.poison.es.domain.Group;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
@@ -11,4 +12,6 @@ public interface GroupMapper {
     void addGroup(Group group);
 
     List<Group> findAllGroups();
+
+    Group findGroupById(@Param("id") Long id);
 }
--- src/main/java/com/poison/es/service/shop/ShopServiceImpl.java
+++ src/main/java/com/poison/es/service/shop/ShopServiceImpl.java
@@ -1,6 +1,9 @@
 package com.poison.es.service.shop;
 
+import com.poison.es.common.error.CodeException;
+import com.poison.es.domain.Group;
 import com.poison.es.domain.Shop;
+import com.poison.es.service.group.GroupMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,15 +15,27 @@ public class ShopServiceImpl implements ShopService {
 
     @Autowired
     private ShopMapper shopMapper;
+    @Autowired
+    private GroupMapper groupMapper;
 
     @Override
     public void addShop(Shop shop) {
+        //查询分组是否存在
+        hasGroup(shop);
         shopMapper.addShop(shop);
     }
 
+    private void hasGroup(Shop shop) {
+        Long groupId = shop.getGroupId();
+        Group group = groupMapper.findGroupById(groupId);
+        if (group == null) {
+            throw new CodeException(-1, "分组不存在或已被删除");
+        }
+    }
+
     @Override
-    public Shop findShopById(Long id){
-        Shop shop=shopMapper.findShopById(id);
+    public Shop findShopById(Long id) {
+        Shop shop = shopMapper.findShopById(id);
         return shop;
     }
 }
